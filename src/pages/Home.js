2017cef5985f78093f8bd1913007cbb8ad530b0d
Danilo Souza <danilos063@gmail.com>
2023-03-07 15:57:09 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,9 @@ function Home() {
             <Row md="auto" style={{height:"100%"}} className="justify-content-center align-items-center">
                 <Col>
                 <h1>Hi! I am Danilo Cortes De Souza</h1>
-                <p style={{fontStyle: "italic"}}>Software Developer</p>
+                <p style={{fontStyle: "italic"}}>Software Developer from Doral, Florida</p>
+                <p style={{fontStyle: "italic"}}>danilos063@gmail.com</p>
+                <p style={{fontStyle: "italic"}}>786-440-4496</p>
                 </Col>
             </Row>
         </Container>
